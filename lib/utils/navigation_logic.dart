10ef0508f69f6d6aa7717b77ff70ec0b2f83319a
adoan-edu <adoan.edu@gmail.com>
2026-04-21 20:15:34 +0800

    wip: refactor navigation packet - pending integration

--- lib/utils/navigation_logic.dart
+++ lib/utils/navigation_logic.dart
@@ -1,53 +1,91 @@
 import '../models/navigation_entity.dart';
+import '../models/navigation_packet.dart';
 import 'dart:math';
 
 // Contains the mathematics and logic that connects the nodes and landmarks
 class NavigationLogic {
   num currentHeading = 0;
 
-void updateNavigation(int currentIndex, List<String> route, Map<String, NavigationEntity> masterData) {
+  NavigationPacket updateNavigation(
+    int currentIndex,
+    List<String> route,
+    Map<String, NavigationEntity> masterData,
+  ) {
     // Get IDs from the route
     // node A->B = incoming link, node B->C = outgoing link
     String idA = (currentIndex > 0) ? route[currentIndex - 1] : "";
     String idB = route[currentIndex];
-    String idC = (currentIndex < route.length - 1) ? route[currentIndex + 1] : "";
+    String idC = (currentIndex < route.length - 1)
+        ? route[currentIndex + 1]
+        : "";
 
     NavigationEntity? nodeA = masterData[idA];
     NavigationEntity? nodeB = masterData[idB];
     NavigationEntity? nodeC = masterData[idC];
 
+    // Data check
+    if (nodeB == null) {
+      throw Exception("Error: Node $idB not found in Master Data");
+    }
+
     // Bound check
+    if (nodeC == null) {
+      return NavigationPacket(
+        current: nodeB,
+        next: null,
+        distance: 0.0,
+        action: "end",
+        landmarks: findLinkLandmarks(nodeB.id, nodeC.id, masterData),
+      );
+    }
+
     if (nodeA == null) {
-      return;
+      return NavigationPacket(
+        current: nodeB,
+        next: nodeC,
+        distance: calculateDistance(nodeB, nodeC),
+        action: "start",
+        landmarks: findLinkLandmarks(nodeB.id, nodeC.id, masterData),
+      );
     }
 
-    if (nodeB != null) {
-      if (nodeC == null) {
-        return;
-      }
-      calculateAction(nodeA, nodeB, nodeC);
-    }
+    return NavigationPacket(
+      current: nodeB,
+      next: nodeC,
+      distance: calculateDistance(nodeB, nodeC),
+      action: calculateAction(nodeA, nodeB, nodeC),
+      landmarks: findLandmarksForLink(nodeB.id, nodeC.id, masterData),
+    );
   }
+}
 
-  // Get action from route data
-  double calculateDistance(NavigationEntity currentNode, NavigationEntity nextNode) {
-    final double xO = currentNode.x;
-    final double yO = currentNode.y;
-    final double xN = nextNode.x;
-    final double yN = nextNode.y;
+void findLandmarksForLink() {}
 
-    return (xN - xO).abs() + (yN - yO).abs(); // Node Link Distance
-  }
+// Get action from route data
+double calculateDistance(
+  NavigationEntity currentNode,
+  NavigationEntity nextNode,
+) {
+  final double xO = currentNode.x;
+  final double yO = currentNode.y;
+  final double xN = nextNode.x;
+  final double yN = nextNode.y;
 
-  String calculateAction(NavigationEntity previousNode, NavigationEntity currentNode, NavigationEntity nextNode) {
-    final double xA = currentNode.x;
-    final double yA = currentNode.y;
-    final double xB = nextNode.x;
-    final double yB = nextNode.y;
+  return (xN - xO).abs() + (yN - yO).abs(); // Node Link Distance
+}
 
-    num theta = atan(
-      (yB - yA).abs() / (xB - xA).abs(),
-    ); // Angle between the x and y components that separate the current NavigationEntity and the next NavigationEntity
-    double angleThreshold = (1 / 2) * pi;
+String calculateAction(
+  NavigationEntity previousNode,
+  NavigationEntity currentNode,
+  NavigationEntity nextNode,
+) {
+  final double xA = currentNode.x;
+  final double yA = currentNode.y;
+  final double xB = nextNode.x;
+  final double yB = nextNode.y;
 
+  num theta = atan(
+    (yB - yA).abs() / (xB - xA).abs(),
+  ); // Angle between the x and y components that separate the current NavigationEntity and the next NavigationEntity
+  double angleThreshold = (1 / 2) * pi;
 }
